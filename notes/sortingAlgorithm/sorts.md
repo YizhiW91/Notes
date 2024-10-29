fefb0f08dbc52d875308846482d103c296831eef
Yizhi Wang <yizhi.wang@staples.com>
2024-10-28 22:32:38 -0700

    Updated merge sort. 10/28

--- notes/sortingAlgorithm/sorts.md
+++ notes/sortingAlgorithm/sorts.md
@@ -8,4 +8,16 @@
 
 
 ## Merge Sort
-* O(nlogN)
+* Complexity based on master theorem - O(nlogN).
+* The recursive process took advantage of preserve order from the previous actions.
+* Similar questions:
+  * Count of Smaller Numbers After Self. #315
+  * 求小和.
+
+## Quick Sort
+* The process is to find one number(could be random), then put the number in the array so that left and right side are less than and greater than the number. Then do the same for the two parts.
+* Time complexity varies based on sampling:
+  * The Best case scenario: O(nlogN). The best scenario is the number selected always equally split. Perfect dichotomy.
+  * The Worst case scenario: O(n2). The worst scenario is the number selected always go to the end of the array, causing sorting the whole array every time.
+* Similar questions:
+  * Sort Colors(Dutch flag) #75
--- src/implementation/MergeSort.java
+++ src/implementation/MergeSort.java
@@ -4,49 +4,36 @@ public class MergeSort{
     // bisect the array, order the sub arrays and merge.
     public static int[] sort(int[] arr){
         if(arr.length == 0) return arr;
-        return sortWithIndex(arr, 0, arr.length-1);        
+        sortWithIndex(arr, 0, arr.length-1); 
+        return arr;     
     }  
 
-    public static int[] sortWithIndex(int[] arr, int L, int R){
-        if(L == R) return arr;
-        int M = (L+R)/2;
+    public static void sortWithIndex(int[] arr, int L, int R){
+        if(L == R) return ;
+        int M = L + ((R - L)>>1);
         sortWithIndex(arr, L, M);
         sortWithIndex(arr, M+1, R);
-        return merge(arr, L, M, R);
+        merge(arr, L, M, R);
     }
         
     // when (L, M) and (M+1, R) are both ordered, merge them together
-    private static int[] merge(int[] arr, int L, int M, int R){
-        if(L == R) return arr;
-
+    private static void merge(int[] arr, int L, int M, int R){
         int[] res = new int[R-L+1];
         int i = L;
         int j = M+1;
         int z = 0;
         while(i <= M && j <= R){
-            if(arr[i] <= arr[j]){
-                res[z] = arr[i];
-                i++;
-            } else {
-                res[z] = arr[j];
-                j++; 
-            }
-            z++;
+            res[z++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
         }
         while(i<=M){
-            res[z] = arr[i];
-            i++;
-            z++;
+            res[z++] = arr[i++];
         }
         while(j<=R){
-            res[z] = arr[j];
-            j++;
-            z++;
+            res[z++] = arr[j++];
         }
         // assign res back to arr
         for(int k=L; k<=R; k++){
             arr[k] = res[k-L];
         }
-        return arr;
     }
 }
